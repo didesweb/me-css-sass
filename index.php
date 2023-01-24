aad2df013443eb419454f68911e950dbbe6d385d
Roque Arnas <roquearnas@gmail.com>
2023-01-24 19:13:43 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>me:css</title>
+	<link rel="stylesheet" href="app/styles/main.css">
+</head>
+<body>
+	<div class="ta-r ta-sm-l">ta-r</div>
+</body>
+</html>
